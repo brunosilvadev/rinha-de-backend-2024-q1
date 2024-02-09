1e35def1144455a581c3ac62eaa41ab83e4f7506
Bruno Silva <brunosilva.dev@gmail.com>
2024-02-08 23:59:35 -0600

    small refactor

---
+++ participantes/alleycatbruno/rinha/Migrations/debugging.sql
@@ -0,0 +1,4 @@
+SELECT * FROM "Clientes"
+DROP TABLE "Clientes"
+DELETE FROM "__EFMigrationsHistory"
+SELECT * FROM pg_tables WHERE schemaname = 'public';
--- participantes/alleycatbruno/rinha/Program.cs
+++ participantes/alleycatbruno/rinha/Program.cs
@@ -5,8 +5,6 @@ using rinha.transacao;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ITransacaoWorker, TransacaoWorker>();
@@ -24,13 +22,6 @@ app.UseSwaggerUI();
 app.MapGet("/clientes", async (RinhaDbContext ctx)=>
 {
     return await ctx.Clientes.ToListAsync();
-});
-
-app.MapPost("/cliente", async (RinhaDbContext ctx, Cliente cliente)=>
-{
-    ctx.Clientes.Add(cliente);
-    await ctx.SaveChangesAsync();
-    return Results.Created($"/clientes/{cliente.Id}", cliente);
 });
 
 app.MapPost("/clientes/{id}/transacoes", async (int id, Transacao txn, ITransacaoWorker worker) =>
